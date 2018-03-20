693f4d14a7a88f0cf6fa1a1443114ba6918ba07b
bibin <286972777@qq.com>
2018-03-20 15:57:52 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,19 @@
+<style scoped>
+  .boys {
+    background-color: red;
+  }
+</style>
+<template>
+
+  <div id="app" class="boys">
+    boys got many things11111111
+  </div>
+
+</template>
+<script>
+
+export default {
+  name: 'boys'
+}
+
+</script>
